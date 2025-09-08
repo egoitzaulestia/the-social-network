361478cf18b3073a4fff3501ae31576fc5dd27db
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-08 17:26:22 +0200

    register almost done

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -22,35 +22,51 @@ const Register = () => {
 
   return (
     <form onSubmit={handleOnSumbit}>
-      <label htmlFor="name">Name</label>
+      <div>
+        <label htmlFor="name">Name</label>
+        <br />
+        <input
+          type="text"
+          placeholder="name"
+          name="name"
+          value={name}
+          onChange={handleOnChange}
+        />
+      </div>
 
-      <input
-        type="text"
-        placeholder="name"
-        value={name}
-        onChange={handleOnChange}
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        placeholder="email"
-        value={email}
-        onChange={handleOnChange}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={handleOnChange}
-      />
-      <label htmlFor="confirmPassword">Confirm Password</label>
-      <input
-        type="password"
-        placeholder="confirm password"
-        value={confirmPassword}
-        onChange={handleOnChange}
-      />
+      <div>
+        <label htmlFor="email">Email</label>
+        <br />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          value={email}
+          onChange={handleOnChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={handleOnChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <br />
+        <input
+          type="password"
+          placeholder="confirm password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleOnChange}
+        />
+      </div>
 
       <button type="submit">Register</button>
     </form>
