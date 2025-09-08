a88245bd8ab32b0fc0e6c4d3e5285a26362a951c
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-08 21:07:23 +0200

    authSlice.js created and App.jsx modified to include the new auth slice.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import "./App.css";
 import Register from "./components/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Login from "./components/Login";
+import ConfirmEmail from "./components/ConfirmEmail";
 
 function App() {
   return (
-    <>
-      <Register />
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/confirm-email" element={<ConfirmEmail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
