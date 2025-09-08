87256ad05011e112e7b6cb64df59931bd34d3824
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-08 21:26:48 +0200

    createAsyncThunk

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,9 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
   user: null,
   token: null,
 };
+
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (userData) => {
+    console.log("Registering user with data:", userData);
+
+    //   async (userData, thunkAPI) => {
+    //     try {
+    //       const response = await fetch("http://localhost:4000/api/register", {
+    //         method: "POST",
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify(userData),
+    //       });
+
+    //       if (!response.ok) {
+    //         const errorData = await response.json();
+    //         return thunkAPI.rejectWithValue(errorData);
+    //       }
+
+    //       const data = await response.json();
+    //       return data;
+    //     } catch (error) {
+    //       return thunkAPI.rejectWithValue({ error: error.message });
+    //     }
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
