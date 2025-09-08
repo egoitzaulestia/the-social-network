8dadb13c4bfefd45bf31e21428ad6684ecea87a7
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-08 22:08:42 +0200

    authSlice.js / register almost ready

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,15 +1,23 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "../../services/authService";
 
+// Initial state
 const initialState = {
   user: null,
   token: null,
 };
 
+// Register user
 export const registerUser = createAsyncThunk(
   "auth/registerUser",
   async (userData) => {
     console.log("from store", userData);
 
+    try {
+      return await authService.register(userData);
+    } catch (error) {
+      console.error(error);
+    }
     //   async (userData, thunkAPI) => {
     //     try {
     //       const response = await fetch("http://localhost:4000/api/register", {
