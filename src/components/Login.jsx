f7c265596e515af9d5b031001c2ea9c893c288d8
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-08 20:06:40 +0200

    Login almost ready

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    console.log(`${name}: ${value}`);
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+  return (
+    <form>
+      <div>
+        <label htmlFor="email">Email</label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          name="email"
+          value={email}
+          onChange={handleOnChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          name="password"
+          value={password}
+          onChange={handleOnChange}
+        />
+      </div>
+    </form>
+  );
 };
 
 export default Login;
