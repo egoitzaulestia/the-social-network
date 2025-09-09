83c1ece4b01a42e30f794df70f8bce6ab82f20a8
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-09 23:38:26 +0200

    authService update

--- src/redux/auth/authService.js
+++ src/redux/auth/authService.js
@@ -1,22 +1,39 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000";
+// const API_URL = "http://localhost:3000";
 
-// Register user
-const register = async (userData) => {
-  console.log("from service", userData);
-  const response = await axios.post(`${API_URL}/users/register`, userData);
-  return response.data;
+// // Register user
+// const register = async (userData) => {
+//   console.log("from service", userData);
+//   const response = await axios.post(`${API_URL}/users/register`, userData);
+//   return response.data;
+// };
+
+// const login = async (userData) => {
+//   const response = await axios.post(`${API_URL}/users/login`, userData);
+
+//   if (response.data) {
+//     localStorage.setItem("user", JSON.stringify(response.data.user));
+//     localStorage.setItem("token", JSON.stringify(response.data.token));
+//   }
+//   return response.data;
+// };
+
+const API_BASE = import.meta.env.VITE_API_BASE ?? "http://localhost:3000";
+const api = axios.create({ baseURL: API_BASE });
+
+export const register = async (userData) => {
+  const { data } = await api.post("/users/register", userData);
+  return data;
 };
 
 const login = async (userData) => {
-  const response = await axios.post(`${API_URL}/users/login`, userData);
-
-  if (response.data) {
-    localStorage.setItem("user", JSON.stringify(response.data.user));
-    localStorage.setItem("token", JSON.stringify(response.data.token));
+  const { data } = await api.post("/users/login", userData);
+  if (data) {
+    localStorage.setItem("user", JSON.stringify(data.user));
+    localStorage.setItem("token", JSON.stringify(data.token));
   }
-  return response.data;
+  return data;
 };
 
 const authService = {
