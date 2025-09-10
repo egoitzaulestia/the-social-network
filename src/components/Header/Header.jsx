e938ba2534d0fd61690538e56ff792ee63064b4a
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-10 22:58:46 +0200

    header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../../redux/auth/authSlice";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const handleOnLogout = (e) => {
+    e.preventDefault();
+    dispatch(logoutUser());
+    navigate("/login");
+  };
   return (
-    <>
+    <nav>
       <h1>Header</h1>
-    </>
+      {user ? (
+        <button onClick={handleOnLogout}>Logout</button>
+      ) : (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </>
+      )}
+    </nav>
   );
 };
 
