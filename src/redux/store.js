2f6e35edc951e5d56041ccd9d0c6697e875cfa7d
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-12 22:06:49 +0200

    store updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import authReducer from "./auth/authSlice";
 import auth from "./auth/authSlice";
-import posts from "./post/postsSlice";
+import posts from "./posts/postsSlice";
 
 export const store = configureStore({
   reducer: { auth, posts },
