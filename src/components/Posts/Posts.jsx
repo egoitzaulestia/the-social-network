f1e30e52cacd6ac71598c8170bf0c03b51ba47c0
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-12 23:00:28 +0200

    VITE_API_BASE correction and renaming getAllFull to getAllPostsInfo

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,7 +1,15 @@
-import React from "react";
 import Post from "../Post/Post";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getAllPostsInfo } from "../../redux/posts/postsSlice";
 
 const Posts = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllPostsInfo());
+  }, []);
+
   return (
     <>
       <h1>Posts</h1>
--- src/redux/posts/postsService.js
+++ src/redux/posts/postsService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-const api = axios.create({ baseURL: API_URL });
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:3000";
+const api = axios.create({ baseURL: API_BASE });
 
 const getAllPostsInfo = async () => {
   const { data } = await api.get("/posts/full");
