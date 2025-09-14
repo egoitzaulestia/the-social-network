1dbfb7e4954aaead44b3dd7f98b18ece17fc730c
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-14 19:41:00 +0200

    Post Service updated

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -13,17 +13,20 @@ const Posts = () => {
 
   return (
     <>
-      {posts && posts.length === 0 ? (
+      {/* {posts && posts.length === 0 ? (
         <p>No posts available</p>
       ) : (
         posts && posts.map((post) => <Post key={post._id} post={post} />)
-      )}{" "}
-      ;
+      )}{" "} */}
+
       {posts &&
         posts.map((post) => (
           <div key={post._id}>
             <h2>{post.title}</h2>
             <p>{post.content}</p>
+            <img src={post.imageUrl} alt={post.title} width="200" />
+            <p>Author: {post.author.name}</p>
+            <p>Created At: {new Date(post.createdAt).toLocaleString()}</p>
             <hr />
           </div>
         ))}
--- src/redux/posts/postsService.js
+++ src/redux/posts/postsService.js
@@ -3,8 +3,10 @@ import axios from "axios";
 const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:3000";
 const api = axios.create({ baseURL: API_BASE });
 
-const getAllPostsInfo = async () => {
-  const { data } = await api.get("/posts/full");
+const getAllPostsInfo = async (page = 1, limit = 10) => {
+  const { data } = await api.get("/posts/full", {
+    params: { page, limit },
+  });
   return data.posts; // Return the data.posts
 };
 
