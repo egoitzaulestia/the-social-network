479748ba7515649160693686d30383ae49c1994d
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-14 19:56:29 +0200

    posts updated

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,41 +1,3 @@
-// import Post from "../Post/Post";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useEffect } from "react";
-// import { getAllPostsInfo } from "../../redux/posts/postsSlice";
-
-// const Posts = () => {
-//   const dispatch = useDispatch();
-//   const { posts } = useSelector((store) => store.posts);
-
-//   useEffect(() => {
-//     dispatch(getAllPostsInfo());
-//   }, []);
-
-//   return (
-//     <>
-//       {/* {posts && posts.length === 0 ? (
-//         <p>No posts available</p>
-//       ) : (
-//         posts && posts.map((post) => <Post key={post._id} post={post} />)
-//       )}{" "} */}
-
-//       {posts &&
-//         posts.map((post) => (
-//           <div key={post._id}>
-//             <h2>{post.title}</h2>
-//             <p>{post.content}</p>
-//             <img src={post.imageUrl} alt={post.title} width="200" />
-//             <p>Author: {post.author.name}</p>
-//             <p>Created At: {new Date(post.createdAt).toLocaleString()}</p>
-//             <hr />
-//           </div>
-//         ))}
-//     </>
-//   );
-// };
-
-// export default Posts;
-
 import Post from "../Post/Post";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
@@ -55,8 +17,8 @@ const Posts = () => {
 
   return (
     <>
-      {posts.map((p) => (
-        <Post key={p._id} post={p} />
+      {posts.map((post) => (
+        <Post key={post._id} post={post} />
       ))}
     </>
   );
