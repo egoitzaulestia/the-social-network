24d806d602efd829cf2261fa21f8bb0df11c752b
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-15 22:20:39 +0200

    PostDetail updated

--- src/components/PostDetail/PostDetail.jsx
+++ src/components/PostDetail/PostDetail.jsx
@@ -1,25 +1,40 @@
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { getPostById } from "../../redux/posts/postsSlice";
+import { getPostById, clearPost } from "../../redux/posts/postsSlice";
 
 const PostDetail = () => {
-  const { post } = useSelector((state) => state.posts);
   const dispatch = useDispatch();
   const { id } = useParams();
   console.log(id);
 
+  const { post, isLoading, isError, message } = useSelector(
+    (state) => state.posts
+  );
+
   useEffect(() => {
-    // dispatch action to get post by id
+    // fetch on mount / id change
     dispatch(getPostById(id));
+    // clear previous post on unmount
+    return () => {
+      dispatch(clearPost());
+    };
   }, [dispatch, id]);
 
-  if (!post) return <p>Loading…</p>;
-  //   if (post.isError) return <p>Error: {post.message}</p>;
+  if (isLoading) return <p>Loading…</p>;
+  if (isError) return <p>Error: {message}</p>;
   if (!post) return <p>No post found</p>;
   console.log(post);
 
-  return <div>PostDetail</div>;
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+      <p>
+        <i>By {post.author?.name || "Unknown author"}</i>
+      </p>
+    </article>
+  );
 };
 
 export default PostDetail;
