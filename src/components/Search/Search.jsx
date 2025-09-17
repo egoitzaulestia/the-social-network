ed664a7deb3afaa2523c5c73a5247f894a720935
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-17 17:01:03 +0200

    search title almost done

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,40 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPostByTitle, reset } from "../../redux/posts/postsSlice";
 
 const Search = () => {
   const { postName } = useParams();
+  const dispatch = useDispatch();
+  const { posts, isLoading, isError, message } = useSelector(
+    (store) => store.posts
+  );
 
   useEffect(() => {
-    console.log("Searching for:", postName);
-  }, [postName]);
+    if (postName) {
+      dispatch(getPostByTitle(postName));
+    }
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [dispatch, postName]);
+
+  if (isLoading) return <p>Loading…</p>;
+  if (isError) return <p>Error: {message}</p>;
+  if (!posts?.length) return <p>No posts found for "{postName}"</p>;
 
   return (
     <>
       <h1>Search</h1>
-      <p>Searching for: {postName}</p>
+      {/* <p>Searching for: {postName}</p> */}
+      {posts.map((post) => (
+        <div key={post._id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+          <hr />
+        </div>
+      ))}
     </>
   );
 };
