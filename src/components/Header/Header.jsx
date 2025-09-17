ce922bf1ee8f9e97c528610770cea072c842b2c7
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-17 16:35:01 +0200

    Search updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../../redux/auth/authSlice";
+import { useState } from "react";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState("");
 
   const { token } = useSelector((state) => state.auth);
 
@@ -13,9 +15,28 @@ const Header = () => {
     dispatch(logoutUser());
     navigate("/");
   };
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+    if (e.key === "Enter") {
+      navigate(`/search/${searchTerm}`);
+
+      console.log(searchTerm);
+    }
+
+    // console.log(e.target.value);
+  };
+
   return (
     <nav>
       <Link to="/">Home</Link>
+      <input
+        onKeyUp={handleChange}
+        type="text"
+        placeholder="Search..."
+        name="search"
+      />
+
       {token ? (
         <>
           <Link to="/profile">Profile</Link>
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { useParams } from "react-router-dom";
 
 const Search = () => {
+  const { postName } = useParams();
   return <h1>Search</h1>;
 };
 
