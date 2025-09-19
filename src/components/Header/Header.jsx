62204ee845da4142691c0f01386c782248a6bb31
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-19 12:35:22 +0200

    scss implementation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../../redux/auth/authSlice";
 import { useState } from "react";
+import logo from "../../assets/the-social-network.svg";
+import "../../assets/sass/main.scss";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const location = useLocation();
   const [searchTerm, setSearchTerm] = useState("");
 
   const { token } = useSelector((state) => state.auth);
@@ -20,35 +23,86 @@ const Header = () => {
     setSearchTerm(e.target.value);
     if (e.key === "Enter") {
       navigate(`/search/${searchTerm}`);
-
       console.log(searchTerm);
     }
+  };
 
-    // console.log(e.target.value);
+  const isActiveLink = (path) => {
+    return location.pathname === path;
   };
 
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <input
-        onKeyUp={handleChange}
-        type="text"
-        placeholder="Search..."
-        name="search"
-      />
+    <header className="header">
+      {/* Left section with logo */}
+      <div className="header__left">
+        <div className="header__logo">
+          <Link to="/" className="header__logo-link">
+            <img 
+              src={logo} 
+              alt="The Social Network" 
+              className="header__logo-image"
+            />
+          </Link>
+        </div>
+      </div>
 
-      {token ? (
-        <>
-          <Link to="/profile">Profile</Link>
-          <button onClick={handleOnLogout}>Logout</button>
-        </>
-      ) : (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </>
-      )}
-    </nav>
+      {/* Center section with search */}
+      <div className="header__center">
+        <div className="header__search">
+          <input
+            onKeyUp={handleChange}
+            type="text"
+            placeholder="Search posts, people, and more..."
+            name="search"
+            className="header__search-input"
+          />
+        </div>
+      </div>
+
+      {/* Right section with navigation */}
+      <div className="header__right">
+        <nav className="header__nav">
+          <Link 
+            to="/" 
+            className={`header__nav-link ${isActiveLink('/') ? 'header__nav-link--active' : ''}`}
+          >
+            Home
+          </Link>
+
+          {token ? (
+            <>
+              <Link 
+                to="/profile" 
+                className={`header__nav-link ${isActiveLink('/profile') ? 'header__nav-link--active' : ''}`}
+              >
+                Profile
+              </Link>
+              <button 
+                onClick={handleOnLogout}
+                className="header__button header__button--secondary"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link 
+                to="/login" 
+                className={`header__nav-link ${isActiveLink('/login') ? 'header__nav-link--active' : ''}`}
+              >
+                Login
+              </Link>
+              <Link 
+                to="/register" 
+                className="header__button header__button--primary"
+              >
+                Register
+              </Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
   );
 };
 
