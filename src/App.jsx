4d45e94af31a596d743d6afa8db584ff4b77dec9
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-19 13:12:03 +0200

    scss update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// Removed App.css - using SASS instead
 import {
   BrowserRouter as Router,
   Routes,
@@ -19,19 +19,21 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          {/* just info */}
-          <Route path="/confirm-email" element={<ConfirmEmail />} />
-          {/* does the call */}
-          <Route path="/confirm/:token" element={<ConfirmEmail />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/post/:id" element={<PostDetail />} />
-          <Route path="/search/:postName" element={<Search />} />
-          <Route path="*" element={<h1>404 NOT FOUND</h1>} />
-        </Routes>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* just info */}
+            <Route path="/confirm-email" element={<ConfirmEmail />} />
+            {/* does the call */}
+            <Route path="/confirm/:token" element={<ConfirmEmail />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/post/:id" element={<PostDetail />} />
+            <Route path="/search/:postName" element={<Search />} />
+            <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+// Removed index.css - using SASS instead
+// import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
